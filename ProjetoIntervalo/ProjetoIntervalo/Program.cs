7a73300193aa100b8a698563f01ba72b4151598e
GabrielBertti <gabrielbertti58@gmail.com>
2020-04-02 23:21:05 -0300

    Iniciou- se o exercicio

---
+++ ProjetoIntervalo/ProjetoIntervalo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProjetoIntervalo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Valor?");
+            double num = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (num < 0.0 || num >100.0)
+            {
+                Console.WriteLine("Fora de Intervalo");
+                Console.ReadKey();
+            }
+            else if (num <= 25.0)
+            {
+                Console.WriteLine("Intervalo [0;25]");
+                Console.ReadKey();
+            }
+            else if (num <= 50.0)
+            {
+                Console.WriteLine("Intervalo [25;50]");
+                Console.ReadKey();
+            }
+            else if (num <= 75.0)
+            {
+                Console.WriteLine("Intervalo [50;75]");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Intervalo [75; 100]");
+                Console.ReadKey();
+            }
+        }
+    }
+}
